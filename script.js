aba3ac064ef0e71fdc5fc730ae41e3eb360c2d9c
Sean Kempt <kempt101@gmail.com>
2022-04-03 01:00:00 -0500

    Added function to run five games and determine winner

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
-/* Function needs to return `Rock` `Paper` or `Scissors` randomly. */
 const choices = [`Rock`, `Paper`, `Scissors`];
-let computerSelection = computerPlay();
-let playerSelection = prompt(`What is your choice?`);
-playerSelection = playerSelection.toUpperCase();
-computerSelection = computerSelection.toUpperCase();
+let computerSelection;
+let playerSelection;
 
 //Function chooses randomly from the array choices.
+
+/* Function needs to return `Rock` `Paper` or `Scissors` randomly. */
 function computerPlay (){
-    return choices[Math.floor(Math.random() * choices.length)];
+    computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    computerSelection = computerSelection.toUpperCase();
 }
 
+function playerChoice () {
+    playerSelection = prompt(`What is your Choice?`);
+    playerSelection = playerSelection.toUpperCase();
+}
 /* function playRockPaperScissors takes playerSelection and computerSelection and return a string that declares the winner of the round as `You lose! Paper
 beats Rock` */
 
 function playRockPaperScissors (playerSelection, computerSelection){
     if (playerSelection === `ROCK` && computerSelection === `PAPER`) {
-        return `You lose! Paper beats rock!`;
+        return console.log(`You lose! Paper beats rock!`);
     } else if (playerSelection === `SCISSORS` && computerSelection === `ROCK`) {
-        return `You lose! Rock Beats paper!`;
+        return console.log(`You lose! Rock Beats paper!`);
     } else if (playerSelection === `PAPER` && computerSelection === `SCISSORS`) {
-        return `You lose! Scissors beats Paper!`;
+        return console.log(`You lose! Scissors beats Paper!`);
     } else if (playerSelection === `ROCK` && computerSelection === `SCISSORS`) {
-        return `You Win! Rock beats Scissors!`;
+        return console.log(`You Win! Rock beats Scissors!`);
     } else if (playerSelection === `PAPER` && computerSelection === `ROCK`) {
-        return `You Win! Paper beats Rock!`;
+        return console.log(`You Win! Paper beats Rock!`);
     } else if (playerSelection === `SCISSORS` && computerSelection === `PAPER`) {
-        return `You Win! Scissors beats Paper!`;
+        return console.log(`You Win! Scissors beats Paper!`);
     } else if (playerSelection === computerSelection) {
-        return `Its a tie! Play again!`;
+        return console.log(`Its a tie! Play again!`);
     } else {
-        return `Try again!`;
+        return console.log(`Try again!`);
     }
 }
 
+// let results = console.log(playRockPaperScissors(playerSelection, computerSelection));
+
+/*Write a NEW function called game(). Call the playRound function inside of this one to play 
+a 5 round game that keeps score and reports a winner or loser at the end. */
+
+function game (){
+    for(let i = 0; i < 5; i++) {
+        playerChoice();
+        computerPlay();
+        playRockPaperScissors(playerSelection, computerSelection);
+    }
+}
