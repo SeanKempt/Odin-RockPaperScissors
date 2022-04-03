04c252ad619a0d2c98326bb8bdb1ba2292d50942
“Kempt101” <Kempt101@gmail.com>
2022-04-02 22:32:28 -0500

    Added function to play a round of Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 /* Function needs to return `Rock` `Paper` or `Scissors` randomly. */
 const choices = [`Rock`, `Paper`, `Scissors`];
+let computerSelection = computerPlay();
+let playerSelection = prompt(`What is your choice?`);
+playerSelection = playerSelection.toUpperCase();
+computerSelection = computerSelection.toUpperCase();
 
-
+//Function chooses randomly from the array choices.
 function computerPlay (){
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+/* function playRockPaperScissors takes playerSelection and computerSelection and return a string that declares the winner of the round as `You lose! Paper
+beats Rock` */
+
+function playRockPaperScissors (playerSelection, computerSelection){
+    if (playerSelection === `ROCK` && computerSelection === `PAPER`) {
+        return `You lose! Paper beats rock!`;
+    } else if (playerSelection === `SCISSORS` && computerSelection === `ROCK`) {
+        return `You lose! Rock Beats paper!`;
+    } else if (playerSelection === `PAPER` && computerSelection === `SCISSORS`) {
+        return `You lose! Scissors beats Paper!`;
+    } else if (playerSelection === `ROCK` && computerSelection === `SCISSORS`) {
+        return `You Win! Rock beats Scissors!`;
+    } else if (playerSelection === `PAPER` && computerSelection === `ROCK`) {
+        return `You Win! Paper beats Rock!`;
+    } else if (playerSelection === `SCISSORS` && computerSelection === `PAPER`) {
+        return `You Win! Scissors beats Paper!`;
+    } else if (playerSelection === computerSelection) {
+        return `Its a tie! Play again!`;
+    } else {
+        return `Try again!`;
+    }
+}
+
