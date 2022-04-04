c9401cbd26964e5d3a7671985b4d018bee8c45b4
Sean Kempt <kempt101@gmail.com>
2022-04-04 14:36:16 -0500

    Add function to determine winner

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const choices = [`Rock`, `Paper`, `Scissors`];
 let computerSelection;
 let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
 //Function chooses randomly from the array choices.
 
@@ -19,21 +21,27 @@ beats Rock` */
 
 function playRockPaperScissors (playerSelection, computerSelection){
     if (playerSelection === `ROCK` && computerSelection === `PAPER`) {
+        computerScore++;
         return console.log(`You lose! Paper beats rock!`);
     } else if (playerSelection === `SCISSORS` && computerSelection === `ROCK`) {
+        computerScore++;
         return console.log(`You lose! Rock Beats paper!`);
     } else if (playerSelection === `PAPER` && computerSelection === `SCISSORS`) {
+        computerScore++;
         return console.log(`You lose! Scissors beats Paper!`);
     } else if (playerSelection === `ROCK` && computerSelection === `SCISSORS`) {
+        playerScore++;
         return console.log(`You Win! Rock beats Scissors!`);
     } else if (playerSelection === `PAPER` && computerSelection === `ROCK`) {
+        playerScore++;
         return console.log(`You Win! Paper beats Rock!`);
     } else if (playerSelection === `SCISSORS` && computerSelection === `PAPER`) {
+        playerScore++;
         return console.log(`You Win! Scissors beats Paper!`);
     } else if (playerSelection === computerSelection) {
         return console.log(`Its a tie! Play again!`);
     } else {
-        return console.log(`Try again!`);
+        return console.log(`Try again! Incorrect entry.`);
     }
 }
 
@@ -44,8 +52,26 @@ a 5 round game that keeps score and reports a winner or loser at the end. */
 
 function game (){
     for(let i = 0; i < 5; i++) {
-        playerChoice();
-        computerPlay();
-        playRockPaperScissors(playerSelection, computerSelection);
+    playerChoice();
+    computerPlay();
+    playRockPaperScissors(playerSelection, computerSelection);
+    }
+}
+
+// if (playerScore < computerScore) {
+//     console.log(`You lose! with a score of ${playerScore} Try again!`);
+// } else if (computerScore < playerScore) {
+//     console.log(`You Win! with a score of ${playerScore} You are the greatest!`);
+// } else {
+//     console.log(`Something broke!`);
+// }
+
+function gameWinner(){
+    if (playerScore < computerScore) {
+        console.log(`You lose! with a score of ${playerScore} Try again!`);
+    } else if (computerScore < playerScore) {
+        console.log(`You Win! with a score of ${playerScore} You are the greatest!`);
+    } else {
+        console.log(`Something broke!`);
     }
 }
